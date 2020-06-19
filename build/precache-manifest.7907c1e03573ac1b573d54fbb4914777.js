b58c0cf87c78c8b88f938ff5f141172ff4cf611a
Lee Choon Siong <choonsiong.lee@gmail.com>
2020-06-19 16:08:28 +0800

    Updated for Heroku deployment

---
+++ build/precache-manifest.7907c1e03573ac1b573d54fbb4914777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec689714c9defde4232fdd5064251e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78148f6365a3ab6a92d4",
+    "url": "/static/js/2.c250aed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c250aed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0e929c45a052db8593",
+    "url": "/static/js/main.b205e2a9.chunk.js"
+  },
+  {
+    "revision": "70967506e22f91c1ea74",
+    "url": "/static/js/runtime-main.b4436dd1.js"
+  }
+]);
