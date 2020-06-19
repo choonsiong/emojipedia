d25936496035860abe0544b6850536ef24caf2b8
Lee Choon Siong <choonsiong.lee@gmail.com>
2020-06-19 16:55:15 +0800

    Updated to use arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+// No need anymore, after use the anonymous function below
 function createEmojiEntry(emojiEntry) {
     return (
         <Entry
@@ -19,7 +20,15 @@ function App() {
             <h1><span>emojipedia</span></h1>
 
             <dl className="dictionary">
-              {emojipedia.map(createEmojiEntry)}
+                {emojipedia.map(emojiEntry => (
+                        <Entry
+                            key={emojiEntry.id}
+                            name={emojiEntry.name}
+                            emoji={emojiEntry.emoji}
+                            description={emojiEntry.meaning}
+                        />
+                    ))
+                }
             </dl>
         </div>
     );
